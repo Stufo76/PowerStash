f9df4e674fcf12140466486469feaf8e9e0c26fa
itractus <karelclijsters@outlook.com>
2019-06-20 09:27:41 +0200

    added AD group export and Get ACL

---
+++ Get ACL.ps1
@@ -0,0 +1,19 @@
+﻿$browse = New-Object System.Windows.Forms.FolderBrowserDialog
+$browse.ShowNewFolderButton = $false
+$browse.SelectedPath = "U:\"
+$browse.Description = "Select a directory"
+$browse.ShowDialog()
+pause
+    #Stores the selected location
+$Location = $browse.SelectedPath
+
+$FolderPath = Get-ChildItem -Directory -Path $browse.SelectedPath -Force
+$Output = @()
+ForEach ($Folder in $FolderPath) {
+$Acl = Get-Acl -Path $Folder.FullName
+ForEach ($Access in $Acl.Access) {
+$Properties = [ordered]@{'Folder Name'=$Folder.FullName;'Group/User'=$Access.IdentityReference;'Permissions'=$Access.FileSystemRights;'Inherited'=$Access.IsInherited}
+$Output += New-Object -TypeName PSObject -Property $Properties            
+}
+}
+$Output | Out-GridView
